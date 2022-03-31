4de2c7ab8973d15239ae91af94baebaf245312fc
Hoang Nguyen IMDEA <ngpbhoang1406@gmail.com>
2022-03-31 12:13:08 +0200

    Adding instructions and visulization

---
+++ scripts/README.md
@@ -0,0 +1,11 @@
+# Some adjustments to the plot
+
+## Making the x-axis appear fully instead of evenly spaced:
+  - https://stackoverflow.com/questions/28506907/how-to-make-matplotlib-show-all-x-coordinates
+  - https://matplotlib.org/2.0.2/api/pyplot_api.html and look for session: matplotlib.pyplot.xticks(*args, **kwargs)
+
+## Moving the legends to the upper area (for now)
+  - https://stackoverflow.com/questions/4700614/how-to-put-the-legend-outside-the-plot-in-matplotlib
+
+## Adding grids
+  - https://stackoverflow.com/questions/20616754/pandas-how-to-display-minor-grid-lines-on-x-axis-in-pd-dataframe-plot
--- scripts/visualize.py
+++ scripts/visualize.py
@@ -3,80 +3,42 @@
 import pandas as pd 
 import matplotlib.pyplot as plt 
 
-FILE_NAME = "Query4Opt"  
+FILE_NAMES = [
+    "Query4full", 
+    "Query5full", 
+    "Query6full"
+]
 
-df = pd.read_csv("../output/res_" + FILE_NAME + ".csv", delimiter=";")
+for file_name in FILE_NAMES:
+    df = pd.read_csv("../output/" + file_name + ".csv", delimiter=";") # Getting the csv file into dataframe
+    df['Scenario'] = pd.to_numeric(df['Scenario'].str[3:]) # Transform the scenario into numeric (i.e. vgu100 -> 100)
+    df = df[['RunName','Scenario','MetricValue']] # Shepherding dataframe to the minimum needed info
+    rs = df.groupby(['RunName','Scenario'], as_index=False).mean() # Calculate the mean based on RunName and Scenario
 
-df = df[['Scenario','ProcedureName','MetricValue']]
+    xs = rs['Scenario'].drop_duplicates().to_numpy() # Setting the x-axis
+    ys = rs['RunName'].drop_duplicates().to_numpy() # Setting the line themes
+    markers = ['o','p','s','*'] # Markers placeholders
 
-# view dataset 
-print(df) 
+    fig = plt.figure() # Creating figures
+    ax = plt.subplot(111) # Adding first subplot (in case I have more, then 2x2 or something)
+    ax.set_xlabel("Database size") # Setting labels
+    ax.set_ylabel("seconds") 
 
-groupby = df[['Scenario','ProcedureName','MetricValue']].groupby(['Scenario','ProcedureName']).agg({'MetricValue': ['mean']}).unstack().reset_index()
+    for i, runname in enumerate(ys): # Creating line by lines
+        lineframe = rs.loc[rs['RunName'] == runname, ['Scenario','MetricValue']] # Getting dataframe for line
+        line, = ax.plot(lineframe['Scenario'], lineframe['MetricValue'], label = runname, lw=1) # plot it!
+        line.set_marker(markers[i])
+        
 
-# df = df[['Scenario','ProcedureCall','MetricValue']]
+    # Shrink current axis's height by 10% on the bottom
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0 + box.height * 0.1, box.width, box.height * 0.8])
+    ax.grid('on', which='minor', axis='y')
+    ax.grid('off', which='major', axis='y')
 
-# # view dataset 
-# print(df) 
+    # Put a legend below current axis
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), fancybox=True, shadow=True, ncol=4, fontsize=8)
 
-# groupby = df[['Scenario','ProcedureCall','MetricValue']].groupby(['Scenario','ProcedureCall']).agg({'MetricValue': ['mean']}).unstack().reset_index()
+    plt.xticks(xs) # Setting x-axis to display fully from 100 to 1000
 
-# view dataset 
-print(groupby)  
-
-# groupby = groupby[::-1]
-
-groupby.plot(
-    x = 'Scenario',
-    y = 'MetricValue'
-)
-
-plt.xlabel("UnivX")
-plt.ylabel("Execution time (seconds)")
-plt.xticks([200,400,600,800,1000],[r'$2*10^2$',r'$4*10^2$',r'$6*10^2$',r'$8*10^2$',r'$10^3$'])
-# plt.xticks(rotation=45)
-
-ax1 = plt.subplot(111)
-
-# ax2 = ax1.twiny()
-# ax1Ticks = ax1.get_xticks() 
-# ax2Ticks = ax1Ticks
-
-# def tick_function(X):
-#     V = X*X
-#     return V
-
-# ax2.set_xticks(ax2Ticks)
-# ax2.set_xbound(ax1.get_xbound())
-# ax2.set_xticklabels(tick_function(ax2Ticks))
-
-# ax2.set_xlabel('Enrollments')
-# ax2.set_xticks([100,200,400,600,800,1000])
-# ax2.set_xticklabels(['10000','40000','160000','360000','640000','1000000'])
-
-
-# plt.xticks([1,2,3,4,5,6])
-
-# markers = ['o','p','<','*']
-
-# for i, line in enumerate(ax1.get_lines()):
-#     line.set_marker(markers[i])
-
-# handles,labels = ax1.get_legend_handles_labels()
-
-# handles = [handles[2], handles[3], handles[1], handles[0]]
-# labels = [
-#     r'$\ulcorner SecQuery(SecVGU$#$X3,Query$#$5) \urcorner (Vinh, Lecturer)$', 
-#     r'$\ulcorner SecQuery(SecVGU$#$X2,Query$#$5) \urcorner (Michel, Lecturer)$', 
-#     r'$\ulcorner SecQuery(SecVGU$#$X1,Query$#$5) \urcorner (Trang, Admin)$', 
-#     'Query#5'
-# ]
-
-# handles = [handles[2], handles[1], handles[0]]
-# labels = ['nornal', 'Scenario2-improved', 'Scenario2']
-
-# ax1.legend(frameon=False, loc='upper left', ncol=4, fontsize=12)
-# ax1.legend(handles,labels,loc=2,frameon=False, fontsize=8)
-
-# plt.savefig("sqlsilineplotC2.pdf")
-plt.savefig("../diagrams/graph_" + FILE_NAME + ".pdf")
+    plt.savefig("../graph_" + file_name + ".pdf") # Save the plot under this name
